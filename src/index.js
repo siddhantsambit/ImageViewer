fe1684601ba240ef4e28f461a9ed9015c3eda5ae
Siddhant Sambit <siddhantsambit@gmail.com>
2019-08-25 19:35:27 +0530

    Header, Login, index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import RouterControl from "./screens/RouterControl";
+//Router Controller for Controlling the routing of Components
+ReactDOM.render(<RouterControl />, document.getElementById("root"));
